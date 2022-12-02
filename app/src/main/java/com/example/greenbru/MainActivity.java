0b4fc450d2deeeb70834b0e99f2dd6b45a35f785
Nawfel Ajari <ajari.nawfel@hotmail.com>
2022-12-02 15:32:38 +0100

    commit

---
+++ app/src/main/java/com/example/greenbru/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.greenbru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals(("admin"))) {
+                    //correct
+                    Toast.makeText(MainActivity.this, "Welkom op GreenBRU!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), GreenBRUMap.class);
+                    startActivity(intent);
+
+                } else
+
+
+                    Toast.makeText(MainActivity.this, "Foute invoer.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
